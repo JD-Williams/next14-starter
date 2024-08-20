e206dbed1d53987bd7ecbc03b6d29be66049ab1d
J.D. Williams <jdwill917+dev@gmail.com>
2024-08-19 20:51:18 -0400

    Updated project configuration files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./home.module.css"
+import { brands, hero } from "@/../public";
+
 const Home = () => {
-  return <div>Hello World!</div>;
-};
+  return (
+    <section className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+        blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src={brands} alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src={hero} alt="" fill className={styles.heroImg} />
+      </div>
+    </section>
+)};
 
 export default Home;
