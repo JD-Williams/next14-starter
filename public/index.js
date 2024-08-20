6fcdb780789fe1eababf3a48652ca77948b480b4
J.D. Williams <jdwill917+dev@gmail.com>
2024-08-19 21:00:42 -0400

    Added static assets for project

---
+++ public/index.js
@@ -0,0 +1,15 @@
+import about from "./about.png"
+import brands from "./brands.png"
+import contact from "./contact.png"
+import hero from "./hero.gif"
+import menu from "./menu.png"
+import noavatar from "./noavatar.png"
+
+export {
+    about,
+    brands,
+    contact,
+    hero,
+    menu,
+    noavatar,
+}
